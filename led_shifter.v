aa9e34cc16874ace8374f303b89eec8e27ca35a0
Ubiratan Tavares <ust1973@gmail.com>
2025-06-16 20:23:06 -0300

    atualização do módulo LedShifter

--- led_shifter.v
+++ led_shifter.v
@@ -6,25 +6,25 @@ module LedShifter #(
     output wire [7:0] leds
 );
 
-    // Registro para armazenar o estado atual dos LEDs
+    // registro para armazenar o estado atual dos LEDs
     reg [7:0] led_reg;
     
-    // Contador para controlar o tempo de deslocamento
+    // contador para controlar o tempo de deslocamento
     reg [31:0] counter;
 
-    // Valor inicial dos LEDS
+    // valor inicial dos LEDS
     localparam INITIAL_LEDS = 8'h1F; // 0b00011111
 
-    // Inicializa registrador e contador
+    // inicializa registrador e contador
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            // Reset: inicializa com 0x1F e zera o contador
+            // reset: inicializa com 0x1F e zera o contador
             led_reg <= INITIAL_LEDS;
             counter <= 0;
         end else begin
-            // Incrementa o contador
+            // incrementa o contador
             if (counter == (CLK_FREQ -1)) begin
-                // Realiza o deslocamento circular para a esquerda
+                // realiza o deslocamento circular para a esquerda
                 led_reg <= {led_reg[6:0], led_reg[7]};
                 counter <= 0;
             end else begin
@@ -33,7 +33,7 @@ module LedShifter #(
         end
     end
     
-    // Atribui o valor do registro à saída
+    // atribui o valor do registro à saída
     assign leds = led_reg;
 
 endmodule
